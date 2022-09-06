dedaf0f22bfafdbda4ed9f4859bea99abb550b09
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-06 19:29:03 +0530

    Hexadecimal to Decimal

---
+++ src/OctalToDecimal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OctalToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a octal number");
+        int num = sc.nextInt();
+
+        int d = 0;
+        int temp = num;
+        int i = 0;
+
+        while (temp > 0) {
+            int r = temp % 10;
+            temp = temp / 10;
+            d = d + r * (int) Math.pow(8, i++);
+        }
+        System.out.println("Decimal of " + num + " is " + d);
+    }
+}
+
+
