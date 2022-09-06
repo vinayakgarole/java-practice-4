0d94483d28a130b3abbccbf6e4ae3f152eeaec4b
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-06 23:36:13 +0530

    Octal to Binary

--- src/OctalToBinary.java
+++ src/OctalToBinary.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class BinaryToOctal {
+public class OctalToBinary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a binary Number: ");
+        System.out.println("Enter a octal Number: ");
         int num=sc.nextInt();
         int d=0;
         int temp=num;
@@ -12,13 +12,13 @@ public class BinaryToOctal {
         while(temp>0) {
             int r=temp%10;
             temp=temp/10;
-            d=d+r*(int) Math.pow(2, i++);
+            d=d+r* (int) Math.pow(8, i++);
         }
         while(d>0) {
-            int r=d%8;
-            d=d/8;
+            int r=d%2;
+            d=d/2;
             b=r+b;
         }
-        System.out.println("octal of " +num+ " is " + b);
+        System.out.println("Binary of " +num+ " is " + b);
     }
 }
